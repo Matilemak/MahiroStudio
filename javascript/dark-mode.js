e81df660acc22af0fd010acf22094c9d8d1f6dea
Matilemak <matias.alcayagac@gmail.com>
2025-03-06 19:50:35 -0300

    arreglos paginas foto y publi, dark mode add

---
+++ javascript/dark-mode.js
@@ -0,0 +1,13 @@
+
+function toggleDarkMode() {
+    const body = document.body;
+    const isDarkMode = body.classList.toggle('dark-mode');
+    localStorage.setItem('darkMode', isDarkMode ? 'enabled' : 'disabled');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const darkMode = localStorage.getItem('darkMode');
+    if (darkMode === 'enabled') {
+        document.body.classList.add('dark-mode');
+    }
+});
